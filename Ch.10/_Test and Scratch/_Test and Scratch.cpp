783a7fec8240760909ce83356838f39de1a7f497
ktjones <todd@ktjones.net>
2014-01-22 20:42:08 -0600

    3

---
+++ Ch.10/_Test and Scratch/_Test and Scratch.cpp
@@ -0,0 +1,22 @@
+// _Test and Scratch.cpp : main project file.
+
+#include "stdafx.h"
+#include <cliext/vector>
+
+using namespace System;
+using namespace cliext;
+
+int main(array<System::String ^> ^args)
+{
+    
+	
+	vector<int> test(10);
+    
+	
+	
+	
+	
+	
+	
+	return 0;
+}

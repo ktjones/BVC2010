db11beb54572b4383f580d6bb3d0e82ac015ff52
ktjones <todd@ktjones.net>
2014-02-02 16:48:59 -0600

    11

--- Ch.15/_Ex15-01 to 04/Sketcher/SketcherDoc.cpp
+++ Ch.15/_Ex15-01 to 04/Sketcher/SketcherDoc.cpp
@@ -22,12 +22,28 @@
 IMPLEMENT_DYNCREATE(CSketcherDoc, CDocument)
 
 BEGIN_MESSAGE_MAP(CSketcherDoc, CDocument)
+	ON_COMMAND(ID_COLOR_BLACK, &CSketcherDoc::OnColorBlack)
+	ON_COMMAND(ID_COLOR_RED, &CSketcherDoc::OnColorRed)
+	ON_COMMAND(ID_COLOR_GREEN, &CSketcherDoc::OnColorGreen)
+	ON_COMMAND(ID_COLOR_BLUE, &CSketcherDoc::OnColorBlue)
+	ON_COMMAND(ID_ELEMENT_LINE, &CSketcherDoc::OnElementLine)
+	ON_COMMAND(ID_ELEMENT_RECTANGLE, &CSketcherDoc::OnElementRectangle)
+	ON_COMMAND(ID_ELEMENT_CIRCLE, &CSketcherDoc::OnElementCircle)
+	ON_COMMAND(ID_ELEMENT_CURVE, &CSketcherDoc::OnElementCurve)
+	ON_UPDATE_COMMAND_UI(ID_ELEMENT_LINE, &CSketcherDoc::OnUpdateElementLine)
+	ON_UPDATE_COMMAND_UI(ID_ELEMENT_RECTANGLE, &CSketcherDoc::OnUpdateElementRectangle)
+	ON_UPDATE_COMMAND_UI(ID_ELEMENT_CIRCLE, &CSketcherDoc::OnUpdateElementCircle)
+	ON_UPDATE_COMMAND_UI(ID_ELEMENT_CURVE, &CSketcherDoc::OnUpdateElementCurve)
+	ON_UPDATE_COMMAND_UI(ID_COLOR_BLACK, &CSketcherDoc::OnUpdateColorBlack)
+	ON_UPDATE_COMMAND_UI(ID_COLOR_RED, &CSketcherDoc::OnUpdateColorRed)
+	ON_UPDATE_COMMAND_UI(ID_COLOR_GREEN, &CSketcherDoc::OnUpdateColorGreen)
+	ON_UPDATE_COMMAND_UI(ID_COLOR_BLUE, &CSketcherDoc::OnUpdateColorBlue)
 END_MESSAGE_MAP()
 
 
 // CSketcherDoc construction/destruction
 
-CSketcherDoc::CSketcherDoc()
+CSketcherDoc::CSketcherDoc() : m_Element(LINE), m_Color(BLACK)
 {
 	// TODO: add one-time construction code here
 
@@ -135,3 +151,99 @@ void CSketcherDoc::Dump(CDumpContext& dc) const
 
 
 // CSketcherDoc commands
+
+
+void CSketcherDoc::OnColorBlack()
+{
+	m_Color = BLACK;
+}
+
+
+void CSketcherDoc::OnColorRed()
+{
+	m_Color = RED;
+}
+
+
+void CSketcherDoc::OnColorGreen()
+{
+	m_Color = GREEN;
+}
+
+
+void CSketcherDoc::OnColorBlue()
+{
+	m_Color = BLUE;
+}
+
+
+void CSketcherDoc::OnElementLine()
+{
+	m_Element = LINE;
+}
+
+
+void CSketcherDoc::OnElementRectangle()
+{
+	m_Element = RECTANGLE;
+}
+
+
+void CSketcherDoc::OnElementCircle()
+{
+	m_Element = CIRCLE;
+}
+
+
+void CSketcherDoc::OnElementCurve()
+{
+	m_Element = CURVE;
+}
+
+
+void CSketcherDoc::OnUpdateElementLine(CCmdUI *pCmdUI)
+{
+	pCmdUI->SetCheck(m_Element==LINE);
+}
+
+
+void CSketcherDoc::OnUpdateElementRectangle(CCmdUI *pCmdUI)
+{
+	pCmdUI->SetCheck(m_Element==RECTANGLE);
+}
+
+
+void CSketcherDoc::OnUpdateElementCircle(CCmdUI *pCmdUI)
+{
+	pCmdUI->SetCheck(m_Element==CIRCLE);
+}
+
+
+void CSketcherDoc::OnUpdateElementCurve(CCmdUI *pCmdUI)
+{
+	pCmdUI->SetCheck(m_Element==CURVE);
+}
+
+
+void CSketcherDoc::OnUpdateColorBlack(CCmdUI *pCmdUI)
+{
+	pCmdUI->SetCheck(m_Color==BLACK);
+}
+
+
+void CSketcherDoc::OnUpdateColorRed(CCmdUI *pCmdUI)
+{
+	pCmdUI->SetCheck(m_Color==RED);
+}
+
+
+void CSketcherDoc::OnUpdateColorGreen(CCmdUI *pCmdUI)
+{
+	pCmdUI->SetCheck(m_Color==GREEN);
+}
+
+
+void CSketcherDoc::OnUpdateColorBlue(CCmdUI *pCmdUI)
+{
+	pCmdUI->SetCheck(m_Color==BLUE);
+}
--- Ch.15/_Ex15-01 to 04/Sketcher/SketcherDoc.h
+++ Ch.15/_Ex15-01 to 04/Sketcher/SketcherDoc.h
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include "SketcherConstants.h"
 
 class CSketcherDoc : public CDocument
 {
@@ -45,4 +46,27 @@ protected:
 	// Helper function that sets search content for a Search Handler
 	void SetSearchContent(const CString& value);
 #endif // SHARED_HANDLERS
+public:
+	afx_msg void OnColorBlack();
+	afx_msg void OnColorRed();
+	afx_msg void OnColorGreen();
+	afx_msg void OnColorBlue();
+	afx_msg void OnElementLine();
+	afx_msg void OnElementRectangle();
+	afx_msg void OnElementCircle();
+	afx_msg void OnElementCurve();
+protected:
+	// Current Element type
+	ElementType m_Element;
+	// Current Color
+	COLORREF m_Color;
+public:
+	afx_msg void OnUpdateElementLine(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateElementRectangle(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateElementCircle(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateElementCurve(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateColorBlack(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateColorRed(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateColorGreen(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateColorBlue(CCmdUI *pCmdUI);
 };

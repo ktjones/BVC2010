0208c06eb25ecc98e6e0429be1bcf6d01691e80f
ktjones <todd@ktjones.net>
2014-02-28 19:03:51 -0600

    24

---
+++ Ch.17/_Ex17-02/Sketcher/Elements.cpp
@@ -0,0 +1,363 @@
+// Element.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "Sketcher.h"
+#include "Elements.h"
+#include <algorithm>
+#include <cmath>
+
+
+// CElement
+
+CElement::CElement()
+{
+}
+
+CElement::~CElement()
+{
+}
+
+
+// CElement member functions
+
+// Get the bounding rectangle for an element
+CRect CElement::GetBoundRect() const
+{
+	CRect boundingRect(m_EnclosingRect); // Object to store bounding rectangle
+	
+	// Increase the rectangle by the pen width and return it
+	boundingRect.InflateRect(m_PenWidth, m_PenWidth);
+	
+	return boundingRect;
+	
+}
+
+
+// CLine
+
+CLine::CLine(void)
+{
+}
+
+// CLine class constructor
+CLine::CLine(const CPoint & start, const CPoint & end, COLORREF aColor, int aPenStyle): m_StartPoint(start), m_EndPoint(end)
+{
+	
+	m_PenWidth = 1; // Set pen width
+	m_LineStyle = aPenStyle;
+	m_Color = aColor; // Set line color
+
+	// Defi ne the enclosing rectangle
+	m_EnclosingRect = CRect(start, end);
+	m_EnclosingRect.NormalizeRect();
+
+}
+
+
+CLine::~CLine(void)
+{
+}
+
+// Draw a CLine object
+void CLine::Draw(CDC* pDC, CElement* pElement)
+{
+	
+	// Create a pen for this object and
+	// initialize it to the object color and line width m_PenWidth
+	CPen aPen;
+	if(!aPen.CreatePen(m_LineStyle, m_PenWidth, this==pElement ? SELECT_COLOR : m_Color))
+	{
+		// Pen creation failed. Abort the program
+		AfxMessageBox(_T("Pen creation failed drawing a line"), MB_OK);
+		AfxAbort();
+	}
+	
+	CPen* pOldPen = pDC->SelectObject(& aPen); // Select the pen
+	
+	// Now draw the line
+	pDC->MoveTo(m_StartPoint);
+	pDC->LineTo(m_EndPoint);
+	pDC->SelectObject(pOldPen); // Restore the old pen
+
+}
+
+void CLine::Move(const CSize & aSize)
+{
+	m_StartPoint += aSize; // Move the start point
+	m_EndPoint += aSize; // and the end point
+	m_EnclosingRect += aSize; // Move the enclosing rectangle
+}
+
+// CRectangle
+
+CRectangle::CRectangle(void)
+{
+}
+
+// CRectangle class constructor
+CRectangle::CRectangle(const CPoint & start, const CPoint & end, COLORREF aColor, int aPenStyle)
+{
+	
+	m_Color = aColor; // Set rectangle color
+	m_LineStyle = aPenStyle;
+	m_PenWidth = 1; // Set pen width
+	
+	// Define the enclosing rectangle
+	m_EnclosingRect = CRect(start, end);
+	m_EnclosingRect.NormalizeRect();
+
+}
+
+CRectangle::~CRectangle(void)
+{
+}
+
+// Draw a CRectangle object
+void CRectangle::Draw(CDC* pDC, CElement* pElement)
+{
+	
+	// Create a pen for this object and
+	// initialize it to the object color and line width of m_PenWidth
+	CPen aPen;
+	if(!aPen.CreatePen(m_LineStyle, m_PenWidth, this==pElement ? SELECT_COLOR : m_Color))
+	{
+		// Pen creation failed
+		AfxMessageBox(_T("Pen creation failed drawing a rectangle"), MB_OK);
+		AfxAbort();
+	}
+	
+	// Select the pen
+	CPen* pOldPen = pDC->SelectObject( & aPen);
+	
+	// Select the brush
+	CBrush* pOldBrush=(CBrush*)pDC->SelectStockObject(NULL_BRUSH);
+	
+	// Now draw the rectangle
+	pDC->Rectangle(m_EnclosingRect);
+	pDC->SelectObject(pOldBrush); // Restore the old brush
+	pDC->SelectObject(pOldPen); // Restore the old pen
+
+}
+
+void CRectangle::Move(const CSize & aSize)
+{
+	m_EnclosingRect+= aSize; // Move the rectangle
+}
+
+// CCircle
+
+CCircle::CCircle(void)
+{
+}
+
+CCircle::CCircle(const CPoint& start, const CPoint& end, COLORREF aColor, int aPenStyle)
+{
+	
+	// First calculate the radius
+	// We use floating point because that is required by
+	// the library function (in cmath) for calculating a square root.
+	long radius = static_cast <long>(sqrt(static_cast <double>((end.x-start.x)*(end.x-start.x)+(end.y-start.y)*(end.y-start.y))));
+
+	// Now calculate the rectangle enclosing
+	// the circle assuming the MM_TEXT mapping mode
+	m_EnclosingRect = CRect(start.x-radius, start.y-radius,start.x+radius, start.y+radius);
+	m_EnclosingRect.NormalizeRect(); // Normalize-in case it's not MM_TEXT
+	m_Color = aColor; // Set the color for the circle
+	m_PenWidth = 1; // Set pen width to 1
+	m_LineStyle = aPenStyle;
+
+}
+
+CCircle::~CCircle(void)
+{
+}
+
+// Draw a circle
+void CCircle::Draw(CDC* pDC, CElement* pElement)
+{
+	
+	// Create a pen for this object and
+	// initialize it to the object color and line width of m_PenWidth
+	CPen aPen;
+	
+	if(!aPen.CreatePen(m_LineStyle, m_PenWidth, this==pElement ? SELECT_COLOR : m_Color))
+	{
+		// Pen creation failed
+		AfxMessageBox(_T("Pen creation failed drawing a circle"), MB_OK);
+		AfxAbort();
+	}
+	
+	// Select the pen
+	CPen* pOldPen = pDC-> SelectObject(& aPen); 
+	
+	// Select a null brush
+	CBrush* pOldBrush = (CBrush*)pDC->SelectStockObject(NULL_BRUSH);
+	
+	// Now draw the circle
+	pDC->Ellipse(m_EnclosingRect);
+	pDC->SelectObject(pOldPen); // Restore the old pen
+	pDC->SelectObject(pOldBrush); // Restore the old brush
+
+}
+
+void CCircle::Move(const CSize & aSize)
+{
+	m_EnclosingRect+= aSize; // Move rectangle defining the circle
+}
+
+// Curve
+
+CCurve::CCurve(void)
+{
+}
+
+CCurve::CCurve(const CPoint& first, const CPoint& second, COLORREF aColor, int aPenStyle)
+{
+
+	// Store the points
+	//m_Points.push_back(first);
+	//m_Points.push_back(second);
+	//m_lPoints.push_front(first);
+	//m_lPoints.push_front(second);
+	m_plPoints.push_front(first);
+	m_plPoints.push_front(second);
+	m_Color = aColor;
+	m_EnclosingRect = CRect(min(first.x, second.x), min(first.y, second.y),max(first.x, second.x), max(first.y, second.y));
+	m_PenWidth = 1;
+	m_LineStyle = aPenStyle;
+
+}
+
+CCurve::~CCurve(void)
+{
+}
+
+// Draw a curve
+void CCurve::Draw(CDC* pDC, CElement* pElement)
+{
+	// Create a pen for this object and
+	// initialize it to the object color and line width of m_PenWidth
+	CPen aPen;
+	
+	if(!aPen.CreatePen(m_LineStyle, m_PenWidth, this==pElement ? SELECT_COLOR : m_Color))
+	{
+		// Pen creation failed
+		AfxMessageBox(_T("Pen creation failed drawing a curve"), MB_OK);
+		AfxAbort();
+	}
+	
+	// Select the pen
+	CPen* pOldPen = pDC->SelectObject(& aPen);
+
+	// Now draw the curve
+	//pDC->MoveTo(m_Points[0]);
+	//pDC->MoveTo(*(m_lPoints.begin()));
+	pDC->MoveTo(m_plPoints.begin());
+
+	/*
+	for(size_t i=1; i<m_Points.size(); ++i)
+	{
+		pDC->LineTo(m_Points[i]);
+	}
+		
+	for(auto i=m_lPoints.begin(); i!=m_lPoints.end(); ++i)
+	{
+		pDC->LineTo(*i);
+	}
+	*/
+
+	for(auto i=m_plPoints.begin(); i!=m_plPoints.end(); ++i)
+	{
+		pDC->LineTo(i);
+	}
+
+	pDC->SelectObject(pOldPen); // Restore the old pen
+}
+
+void CCurve::Move(const CSize & aSize)
+{
+	m_EnclosingRect += aSize; // Move the rectangle
+	// Now move all the points
+	//std::for_each(m_Points.begin(), m_Points.end(),[& aSize](CPoint& p){p+=aSize;});
+	//std::for_each(m_lPoints.begin(), m_lPoints.end(),[& aSize](CPoint& p){p+=aSize;});
+	std::for_each(m_plPoints.begin(), m_plPoints.end(),[& aSize](CPoint& p){p+=aSize;});
+}
+
+// Add a segment to the curve
+void CCurve::AddSegment(const CPoint& point)
+{
+	
+	//m_Points.push_back(point); // Add the point to the end
+	//m_lPoints.push_back(point); // Add the point to the end
+	m_plPoints.push_back(point); // Add the point to the end
+
+	// Modify the enclosing rectangle for the new point
+	m_EnclosingRect=CRect(min(point.x,m_EnclosingRect.left),min(point.y,m_EnclosingRect.top),max(point.x,m_EnclosingRect.right),max(point.y,m_EnclosingRect.bottom));
+
+}
+
+// CEllipse
+
+CEllipse::CEllipse(void)
+{
+}
+
+CEllipse::CEllipse(const CPoint& center, const CPoint& end, COLORREF aColor, int aPenStyle)
+{
+	
+	// First calculate the left rect coords (start)
+	// We use floating point because that is required by
+	// the library function (in cmath) for calculating a square root.
+	
+	CPoint start(center);
+	start.Offset(center.x-end.x,center.y-end.y);
+
+	// Now calculate the rectangle enclosing
+	// the circle assuming the MM_TEXT mapping mode
+	
+
+	m_EnclosingRect = CRect(start,end);
+	m_EnclosingRect.NormalizeRect(); // Normalize-in case it's not MM_TEXT
+	m_Color = aColor; // Set the color for the circle
+	m_PenWidth = 1; // Set pen width to 1
+	m_LineStyle = aPenStyle;
+
+}
+
+CEllipse::~CEllipse(void)
+{
+}
+
+// Draw a circle
+void CEllipse::Draw(CDC* pDC, CElement* pElement)
+{
+	
+	// Create a pen for this object and
+	// initialize it to the object color and line width of m_PenWidth
+	CPen aPen;
+	
+	if(!aPen.CreatePen(m_LineStyle, m_PenWidth, this==pElement ? SELECT_COLOR : m_Color))
+	{
+		// Pen creation failed
+		AfxMessageBox(_T("Pen creation failed drawing a circle"), MB_OK);
+		AfxAbort();
+	}
+	
+	// Select the pen
+	CPen* pOldPen = pDC->SelectObject(& aPen); 
+	
+	// Select a null brush
+	CBrush* pOldBrush = (CBrush*)pDC->SelectStockObject(NULL_BRUSH);
+	
+	// Now draw the circle
+	pDC->Ellipse(m_EnclosingRect);
+	pDC->SelectObject(pOldPen); // Restore the old pen
+	pDC->SelectObject(pOldBrush); // Restore the old brush
+
+}
+
+void CEllipse::Move(const CSize & aSize)
+{
+	m_EnclosingRect+= aSize; // Move rectangle defining the circle
+}
---
+++ Ch.17/_Ex17-02/Sketcher/Elements.h
@@ -0,0 +1,114 @@
+#pragma once
+#include "atltypes.h"
+#include <vector>
+#include <list>
+
+// CElement command target
+
+#pragma once
+
+// CElement command target
+
+class CElement : public CObject
+{
+	protected:
+		int m_PenWidth;		// Pen Width
+		int m_LineStyle;	// Line Style
+		COLORREF m_Color;	// Color of an element
+		CRect m_EnclosingRect;	// Rectangle enclosing an element
+
+	public:
+		virtual ~CElement();
+		virtual void Draw(CDC* pDC, CElement* pElement=nullptr) {}	// Virtual draw operation
+		CRect GetBoundRect() const;		// Get the bounding rectangle for an element
+		virtual void Move(const CSize & aSize){} // Move an element
+
+	protected:
+		CElement();			// Here to prevent it from being called
+	
+};
+
+class CLine : public CElement
+{
+	public:
+		virtual ~CLine(void);
+		virtual void Draw(CDC* pDC, CElement* pElement=nullptr);
+		virtual void Move(const CSize & aSize); // Function to move an element
+	
+		// Constructor for a line object
+		CLine(const CPoint& start, const CPoint& end, COLORREF aColor, int aPenStyle);
+
+
+	protected:
+		CPoint m_StartPoint; // Start point of line
+		CPoint m_EndPoint; // End point of line
+	
+		CLine(void);
+
+};
+
+class CRectangle : public CElement
+{
+	public:
+		virtual ~CRectangle(void);
+		virtual void Draw(CDC* pDC, CElement* pElement=nullptr); // Function to display a rectangle
+		virtual void Move(const CSize & aSize); // Function to move an element
+
+		// Constructor for a rectangle object
+		CRectangle(const CPoint& start, const CPoint& end, COLORREF aColor, int aPenStyle);
+
+	protected:
+		CRectangle(void);	// Default constructor - should not be used
+
+};
+
+class CCircle : public CElement
+{
+	public:
+		virtual ~CCircle(void);
+		virtual void Draw(CDC* pDC, CElement* pElement=nullptr); // Function to display a rectangle
+		virtual void Move(const CSize & aSize); // Function to move an element
+
+		// Constructor for a rectangle object
+		CCircle(const CPoint& start, const CPoint& end, COLORREF aColor, int aPenStyle);
+
+	protected:
+		CCircle(void);
+
+
+};
+
+class CCurve : public CElement
+{
+	public:
+		virtual ~CCurve(void);
+		virtual void Draw(CDC* pDC, CElement* pElement=nullptr); // Function to display a curve
+		virtual void Move(const CSize & aSize); // Function to move an element
+
+		// Constructor for a curve object
+		CCurve(const CPoint& first, const CPoint& second, COLORREF aColor, int aPenStyle);
+		void AddSegment(const CPoint& point); // Add a segment to the curve
+
+	protected:
+			
+		//std::vector<CPoint> m_Points; // Points defining the curve using a vector
+		//std::list<CPoint> m_lPoints;  // Points defining the curve using a list 
+		std::list<CPoint*> m_plPoints;  // Points defining the curve using a list 
+		CCurve(void);
+};
+
+class CEllipse : public CElement
+{
+
+	public:
+		virtual ~CEllipse(void);
+		virtual void Draw(CDC* pDC, CElement* pElement=nullptr); // Function to display a rectangle
+		virtual void Move(const CSize & aSize); // Function to move an element
+
+		// Constructor for a rectangle object
+		CEllipse(const CPoint& center, const CPoint& end, COLORREF aColor, int aPenStyle);
+
+	protected:
+		CEllipse(void);	// Default constructor - should not be used
+
+};
